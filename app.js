9d78535748c32b55b19877e26939853c1a59e8b5
ArttuKorpela <122992707+ArttuKorpela@users.noreply.github.com>
2023-12-18 21:45:23 +0200

    yes

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function checkAuthenticated(req, res, next) {
 
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
-        return res.redirect("/")
+        return res.redirect("/api/sercret")
         
     }
     return next()
